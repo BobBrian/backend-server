267b2e805ba6356fc8587746a382394571d604c5
BobBrian <rellrocker235@gmail.com>
2026-02-04 07:06:42 -0330

    Added SQLite but it is experimental in future builds use  sqlite3 and change the code based on that.

--- src/db.js
+++ src/db.js
@@ -1,24 +1,24 @@
-// Create a Lightweight Database
-import { DatabaseSync } from 'node:sqlite'
-const db = new DatabaseSync(':memory:')
+// src/db.js
+import { DatabaseSync } from "node:sqlite"
 
-// Execute SQL statements from strings
+const db = new DatabaseSync(":memory:")
+
 db.exec(`
-    CREATE TABLE users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE,
-        password TEXT
-    )
+  CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+  )
 `)
 
 db.exec(`
-    CREATE TABLE todos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER,
-        task TEXT,
-        completed BOOLEAN DEFAULT 0,
-        FOREIGN KEY(user_id) REFERENCES users(id)
-    )    
+  CREATE TABLE todos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    task TEXT NOT NULL,
+    completed INTEGER DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+  )
 `)
 
 export default db
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import path,{dirname} from 'path'
 import { fileURLToPath } from 'url'
 import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -28,6 +29,7 @@ app.get('/', (req, res) => {
 
 //Routes
 app.use('/auth', authRoutes)
+app.use('/todos', todoRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server has started on port ${PORT}`)
