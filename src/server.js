7e5af5abf6406c9e0d4a6ae3e42b177582bcb65a
BobBrian <rellrocker235@gmail.com>
2026-02-03 21:02:11 -0330

    Add src folder and code

---
+++ src/server.js
@@ -0,0 +1,34 @@
+// The address of this server connected to the network is
+import express from 'express'
+import path,{dirname} from 'path'
+import { fileURLToPath } from 'url'
+import authRoutes from './routes/authRoutes.js'
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+//Get the file path from the url of the module
+const __filename = fileURLToPath(import.meta.url)
+// Get the directory name of the file path
+const __dirname = dirname(__filename)
+
+//Middleware
+app.use(express.json())
+// Serves the HTML file from the /public direcotry
+// Tell express to serve all files from the /public directory as static assests / files
+// Any requests for the css files will be resolved by the public directory
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+// Serving Up HTML files from the .public directory
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','index.html'))
+})
+
+//Routes
+app.use('/auth', authRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`)
+})
